31721cd9f2500c7aeed756ecc238a4536b7b5e0f
sayf <sayf@multicode.nl>
2024-11-01 21:51:24 +0100

    updated the swagger serverlist

--- application/src/main/java/nl/multicode/scraper/config/ApplicationBeanConfig.java
+++ application/src/main/java/nl/multicode/scraper/config/ApplicationBeanConfig.java
@@ -1,5 +1,7 @@
 package nl.multicode.scraper.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import nl.multicode.scraper.client.JsoupClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,5 +13,15 @@ public class ApplicationBeanConfig {
     public JsoupClient getClient() {
 
         return new JsoupClient();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        final var openapi = new OpenAPI();
+        openapi.addServersItem(new Server().url("https://fundamunicipalityscraper.multicode.nl"));
+        openapi.addServersItem(new Server().url("http://fundamunicipalityscraper.multicode.nl"));
+        openapi.addServersItem(new Server().url("http://localhost:8080"));
+        return openapi;
     }
 }
